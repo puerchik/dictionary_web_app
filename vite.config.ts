18b47607d41f21f12dc112ba8f0698cb2e45e48d
puerchik <rumyantsevnn@yandex.ru>
2024-06-03 10:04:20 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      include: "**/*.svg?react",
+    }),
+  ],
+  base: "/dictionary-web-app/",
+  resolve: {
+    alias: {},
+  },
+  server: {
+    port: 3000,
+  },
+});
