130f3f88c747d2a81a75164c9fc098b72d269643
puerchik <rumyantsevnn@yandex.ru>
2024-06-04 08:00:32 +0300

    init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "app/App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import "./index.css";
+import App from 'app/App'
 
-const container = document.getElementById("root")!;
-const root = ReactDOM.createRoot(container);
+const container = document.getElementById('root')!
+const root = ReactDOM.createRoot(container)
 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      include: "**/*.svg?react",
-    }),
-  ],
-  base: "/dictionary-web-app/",
   resolve: {
     alias: {
-      app: "/src/app",
+      app: path.resolve(__dirname, './src/app'),
     },
   },
-  server: {
-    port: 3000,
-  },
-});
+  plugins: [react()],
+})
