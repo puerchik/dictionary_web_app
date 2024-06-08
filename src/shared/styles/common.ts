768146ec2496182a4dec0ca40425ae7668025d08
puerchik <rumyantsevnn@yandex.ru>
2024-06-08 18:59:23 +0300

    base header style

--- src/shared/styles/common.ts
+++ src/shared/styles/common.ts
@@ -2,6 +2,5 @@ import { css } from 'styled-components'
 
 export const Flex = css`
   display: flex;
-  justify-content: space-between;
   align-items: center;
 `
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components'
 
-import LogoIcon from 'shared/ui/icons/dictionary.svg'
+import NativeSelect from '@mui/material/NativeSelect'
+import Switch from '@mui/material/Switch'
 
 import { Container } from 'shared/styles/Conatiner'
-import NativeSelect from '@mui/material/NativeSelect'
 import { Flex } from 'shared/styles/common'
+
+import LogoIcon from 'shared/ui/icons/dictionary.svg'
+import MoonIcon from 'shared/ui/icons/moon.svg'
 
 export const Header = () => {
   return (
@@ -16,14 +19,17 @@ export const Header = () => {
           <S.SerifSelect
             defaultValue={30}
             inputProps={{
-              name: 'age',
+              name: 'font-family',
               id: 'uncontrolled-native',
             }}
           >
-            <option value={10}>Ten</option>
-            <option value={20}>Twenty</option>
-            <option value={30}>Thirty</option>
+            <option value={'serif'}>Serif</option>
+            <option value={'sans-serif'}>Sans serif</option>
           </S.SerifSelect>
+          <S.FlexWrapper>
+            <S.ThemeSelect />
+            <S.ThemeSelectImage />
+          </S.FlexWrapper>
         </S.HeaderInner>
       </S.Header>
     </>
@@ -31,10 +37,15 @@ export const Header = () => {
 }
 
 const S = {
-  Header: styled.header``,
+  Header: styled.header`
+    padding-top: 50px;
+    padding-bottom: 25px;
+  `,
 
   HeaderInner: styled(Container)`
     ${Flex}
+
+    column-gap: 30px;
   `,
 
   Logo: styled.img.attrs(_ => ({
@@ -45,8 +56,36 @@ const S = {
   `,
 
   SerifSelect: styled(NativeSelect)`
+    margin-left: auto;
+
     &::before {
       content: none !important;
+    }
+  `,
+
+  ThemeSelect: styled(Switch)``,
+
+  ThemeSelectImage: styled.img.attrs(_ => ({
+    src: MoonIcon,
+  }))`
+    height: 25px;
+    aspect-ratio: 1;
+  `,
+
+  FlexWrapper: styled.div`
+    position: relative;
+    ${Flex}
+    column-gap: 5px;
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: -15px;
+      translate: 0 -50%;
+      width: 1px;
+      height: 70%;
+      background-color: #808080;
     }
   `,
 }
