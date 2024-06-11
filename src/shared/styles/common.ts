cf45456737d13265a5468ff1e3972cd8c674120c
puerchik <rumyantsevnn@yandex.ru>
2024-06-11 19:35:44 +0300

    input styles

--- src/shared/styles/common.ts
+++ src/shared/styles/common.ts
@@ -4,3 +4,22 @@ export const Flex = css`
   display: flex;
   align-items: center;
 `
+
+export const VisuallyHidden = css`
+  position: absolute !important;
+  width: 1px !important;
+  height: 1px !important;
+  margin: -1px !important;
+  border: 0 !important;
+  padding: 0 !important;
+  white-space: nowrap !important;
+  clip-path: inset(100%) !important;
+  clip: rect(0 0 0 0) !important;
+  overflow: hidden !important;
+`
+
+export const ResetButton = css`
+  padding: 0;
+  background-color: transparent;
+  border: none;
+`
